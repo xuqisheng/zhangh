5744457b14a2436b78415411226b6a878587cd5f
zhangh <huilead@126.com>
2017-03-14 22:01:59 +0800

    zhangYs

---
+++ hpy/PyLinux/LinuxUserDel.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os
+
+user_list  = ['adm','lp','sync','shutdown','halt','news','uucp','opertor','games','gopher']
+group_list = ['adm','lp','news','uucp','games','dip','pppuers','popusers','slipusers']
+
+print type(user_list),type(group_list)
+
+if
+if
