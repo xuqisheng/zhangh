c14b28c4c4aebca8c8f0dc358348d36cb677337a
zhangh <huilead@126.com>
2017-03-14 23:35:00 +0800

    Ys

--- hpy/PyLinux/LinuxBaseMaint.py
+++ hpy/PyLinux/LinuxBaseMaint.py
@@ -8,7 +8,7 @@ user_list  = ['adm','lp','sync','halt','news','uucp','opertor','games','gopher']
 group_list = ['adm','lp','news','uucp','games','dip','pppuers','popusers','slipusers']
 user_dels  = []
 group_dels = []
-
+# 删除不必要的用户
 with open('/etc/passwd','r') as f:
     for line in f:
         for user in user_list:
@@ -18,7 +18,7 @@ with open('/etc/passwd','r') as f:
 for u in user_dels:
     cmd1 = "userdel " + u
     os.system(cmd1)
-
+# 删除不必要的用户组
 with open('/etc/group','r') as f:
     for line in f:
         for group in user_list:
