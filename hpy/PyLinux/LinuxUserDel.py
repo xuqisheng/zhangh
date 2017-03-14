d3623f00e7961ce8ba71d3a848544e8ff6aa20d8
zhangh <huilead@126.com>
2017-03-14 23:32:54 +0800

    Ys

--- hpy/PyLinux/LinuxUserDel.py
+++ hpy/PyLinux/LinuxUserDel.py
@@ -7,6 +7,7 @@ import re
 user_list  = ['adm','lp','sync','halt','news','uucp','opertor','games','gopher']
 group_list = ['adm','lp','news','uucp','games','dip','pppuers','popusers','slipusers']
 user_dels  = []
+group_dels = []
 
 with open('/etc/passwd','r') as f:
     for line in f:
@@ -17,3 +18,14 @@ with open('/etc/passwd','r') as f:
 for u in user_dels:
     cmd1 = "userdel " + u
     os.system(cmd1)
+
+with open('/etc/group','r') as f:
+    for line in f:
+        for group in user_list:
+            if re.match(group,line):
+                user_dels.append(group)
+
+for g in user_dels:
+    cmd2 = "groupdel " + g
+    os.system(cmd2)
+
