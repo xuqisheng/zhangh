3492a02161fa9b14776cca0cf1ae4bb26538dfe1
zhangh <huilead@126.com>
2017-03-27 09:26:04 +0800

    zhangYs

--- Gcsql/up_ihotel_check_repjie.sql
+++ Gcsql/up_ihotel_check_repjie.sql
@@ -3,153 +3,153 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS `up_ihotel_check_repjie`$$
 
 CREATE DEFINER=`root`@`%` PROCEDURE `up_ihotel_check_repjie`(
-			IN arg_hotel_group_id 	INT,  	
-			IN arg_hotel_id 		INT   		
+			IN arg_hotel_group_id 	INT,
+			IN arg_hotel_id 		INT
 )
     SQL SECURITY INVOKER
 label_0:
 BEGIN
 	-- =============================================================================
-	-- µ×±íÅäÖÃ¼ì²é
+	-- åº•è¡¨é…ç½®æ£€æŸ¥
 	-- =============================================================================
-	
-	-- ¶¨Òå±äÁ¿
-	DECLARE var_msg  		VARCHAR(100); 	--  ´íÎóĞÅÏ¢
-	DECLARE var_tmp 		VARCHAR(800); 	--  ´úÂëÅäÖÃÃ÷Ï¸	
-	DECLARE var_tmp_1 		VARCHAR(800); 	--  ´úÂëÅäÖÃÃ÷Ï¸	
-	DECLARE var_tmp_4 		VARCHAR(800); 	--  ´úÂëÅäÖÃÃ÷Ï¸
-	
-	DECLARE var_modeno 		VARCHAR(800); 	--  ´úÂëÅäÖÃÃ÷Ï¸
-	DECLARE var_classno		VARCHAR(60); 	--  µ×±í±àºÅ
-	DECLARE var_descript	VARCHAR(60); 	--  ÃèÊöÏîÄ¿
-	DECLARE var_toop		VARCHAR(60); 	--  ÀÛ¼Æ·½Ê½
-	DECLARE var_toclass		VARCHAR(60);	--  ÀÛ¼Ó¶ÔÏó
-	
+
+	-- å®šä¹‰å˜é‡
+	DECLARE var_msg  		VARCHAR(100); 	--  é”™è¯¯ä¿¡æ¯
+	DECLARE var_tmp 		VARCHAR(800); 	--  ä»£ç é…ç½®æ˜ç»†
+	DECLARE var_tmp_1 		VARCHAR(800); 	--  ä»£ç é…ç½®æ˜ç»†
+	DECLARE var_tmp_4 		VARCHAR(800); 	--  ä»£ç é…ç½®æ˜ç»†
+
+	DECLARE var_modeno 		VARCHAR(800); 	--  ä»£ç é…ç½®æ˜ç»†
+	DECLARE var_classno		VARCHAR(60); 	--  åº•è¡¨ç¼–å·
+	DECLARE var_descript	VARCHAR(60); 	--  æè¿°é¡¹ç›®
+	DECLARE var_toop		VARCHAR(60); 	--  ç´¯è®¡æ–¹å¼
+	DECLARE var_toclass		VARCHAR(60);	--  ç´¯åŠ å¯¹è±¡
+
 	DECLARE var_qx_code  VARCHAR(60);
 	DECLARE var_code_descript  VARCHAR(60);
 	DECLARE var_mktcode  VARCHAR(200);
-	
-	-- ·ÑÓÃÂë
+
+	-- è´¹ç”¨ç 
 	DECLARE var_code_tr   VARCHAR(60);
 	DECLARE var_code_buff VARCHAR(600);
-	
+
 	DECLARE done_cursor     INT DEFAULT 0;
-	-- ¶¨ÒåÓÎ±ê
+	-- å®šä¹‰æ¸¸æ ‡
 	DECLARE done_cursor  CURSOR FOR SELECT modeno,classno,descript,toop,toclass FROM rep_jie WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id ;
-			
-	-- ¶¨ÒåÓÎ±ê½áÊø±êÖ¾
+
+	-- å®šä¹‰æ¸¸æ ‡ç»“æŸæ ‡å¿—
 	DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done_cursor = 1;
-	-- Ìø³ö
-	
+	-- è·³å‡º
+
     DROP TEMPORARY TABLE IF EXISTS role_table_2016;
 	CREATE TEMPORARY TABLE role_table_2016
 	(
-		modeno			VARCHAR(800), 	-- ´úÂëÅäÖÃÃ÷Ï¸
+		modeno			VARCHAR(800), 	-- ä»£ç é…ç½®æ˜ç»†
 		mktcode			VARCHAR(200)	DEFAULT NULL,
-		classno 		VARCHAR(60), 	-- µ×±í±àºÅ
-		descript 		VARCHAR(60), 	-- ÃèÊöÏîÄ¿
-		toop			VARCHAR(60), 	-- ÀÛ¼Æ·½Ê½
-		toclass 		VARCHAR(60)  	-- ÀÛ¼Ó¶ÔÏó
-	); 
+		classno 		VARCHAR(60), 	-- åº•è¡¨ç¼–å·
+		descript 		VARCHAR(60), 	-- æè¿°é¡¹ç›®
+		toop			VARCHAR(60), 	-- ç´¯è®¡æ–¹å¼
+		toclass 		VARCHAR(60)  	-- ç´¯åŠ å¯¹è±¡
+	);
 	DROP TEMPORARY TABLE IF EXISTS role_table_2017;
 	CREATE TEMPORARY TABLE role_table_2017
 	(
-		CODE 	VARCHAR(60) 
+		CODE 	VARCHAR(60)
 	);
-	
-	-- ¿ªÆôÓÎ±ê½øĞĞÊ×´ÎÈ¡Öµ
+
+	-- å¼€å¯æ¸¸æ ‡è¿›è¡Œé¦–æ¬¡å–å€¼
 	OPEN done_cursor;
 	FETCH  done_cursor  INTO  var_modeno,var_classno,var_descript,var_toop,var_toclass;
-	-- ÉèÖÃwhileÑ­»·Ìõ¼ş±ê×¼
+	-- è®¾ç½®whileå¾ªç¯æ¡ä»¶æ ‡å‡†
 	SET done_cursor = 0 ;
 	SET var_tmp_4 = '';
 	WHILE done_cursor = 0 DO
 	     BEGIN
-		  -- 1 Ìæ»» £ºÎª ; 
+		  -- 1 æ›¿æ¢ ï¼šä¸º ;
 		  SET var_modeno = REPLACE(var_modeno,':',';');
-		  -- 3 Ìæ»», Îª '' 
+		  -- 3 æ›¿æ¢, ä¸º ''
 		  SET var_modeno = REPLACE(var_modeno,',',';');
-		  -- Ä©Î² ²¹ ;
+		  -- æœ«å°¾ è¡¥ ;
 		  SET var_modeno = CONCAT(var_modeno,';');
-		  -- Ìæ»»GCNULL Îª '' 
-		  
+		  -- æ›¿æ¢GCNULL ä¸º ''
+
 		  SET var_modeno = REPLACE(var_modeno,'GCNULL;','');
-		  
-		  -- ·­Òë modeno
+
+		  -- ç¿»è¯‘ modeno
 		  SET var_tmp = var_modeno ;
 		  SET var_tmp_1 = '';
 		  SET var_mktcode = '';
-		  
-		 
-		  IF var_tmp <>'' OR LENGTH(var_tmp)>0 OR var_tmp =';'  THEN  		  
+
+
+		  IF var_tmp <>'' OR LENGTH(var_tmp)>0 OR var_tmp =';'  THEN
 			SET var_qx_code = 'To_be_Number_One';
 			WHILE var_tmp <> var_qx_code DO
 				BEGIN
-					 -- ×Ö¶Î½ØÈ¡
-					 IF   var_qx_code = 'To_be_Number_One' THEN 
+					 -- å­—æ®µæˆªå–
+					 IF   var_qx_code = 'To_be_Number_One' THEN
 						SET var_qx_code = SUBSTRING_INDEX(var_tmp,';',1);
 					 ELSE
 						SET var_tmp = REPLACE(var_tmp,CONCAT (var_qx_code,';'),'');
 						SET var_qx_code = SUBSTRING_INDEX(var_tmp,';',1);
 					END IF;
-					
-					-- ÌŞ³ıÖØ¸´
-					INSERT role_table_2017(CODE) SELECT TRIM(var_qx_code);
-					-- ·­Òë´úÂë
 
-					-- ·­Òë´úÂë
-					-- ·­Òë·ÑÓÃÂë
-					
+					-- å‰”é™¤é‡å¤
+					INSERT role_table_2017(CODE) SELECT TRIM(var_qx_code);
+					-- ç¿»è¯‘ä»£ç 
+
+					-- ç¿»è¯‘ä»£ç 
+					-- ç¿»è¯‘è´¹ç”¨ç 
+
 					-- IF  EXISTS(SELECT 1 FROM code_transaction WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND CODE = var_qx_code) then
-					   SELECT descript INTO var_code_descript 
+					   SELECT descript INTO var_code_descript
 							FROM code_transaction
-							WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND CODE = var_qx_code;					
-					-- ÊĞ³¡					
+							WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND CODE = var_qx_code;
+					-- å¸‚åœº
 					IF EXISTS(SELECT 1 FROM code_base WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND  parent_code = 'market_code' AND CODE = var_qx_code) THEN
-					  SELECT CONCAT(var_mktcode,'[',var_qx_code,']',descript,';') INTO var_mktcode 
+					  SELECT CONCAT(var_mktcode,'[',var_qx_code,']',descript,';') INTO var_mktcode
 							FROM code_base
 							WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND parent_code = 'market_code' AND CODE = var_qx_code;
-					 END IF;	
+					 END IF;
 					IF LENGTH(var_qx_code) > 0 AND var_qx_code <> ';'THEN
 						SET var_tmp_1 = CONCAT(var_tmp_1,'[',var_qx_code,']',var_code_descript,';');
 					END IF ;
 					SET var_code_descript = '';
-					
+
 				END;
 			END WHILE;
-		
+
 		  END IF;
-			
+
 		  INSERT INTO role_table_2016(modeno,mktcode,classno,descript,toop,toclass)
 		  VALUES(var_tmp_1,var_mktcode,var_classno,var_descript,var_toop,var_toclass);
-		  
-		  
+
+
 		  SET done_cursor = 0 ;
 		  FETCH  done_cursor   INTO  var_modeno,var_classno,var_descript,var_toop,var_toclass;
-	     END;	
+	     END;
 	END WHILE ;
 	CLOSE done_cursor;
-     
-      -- ¼ì²é·ÑÓÃÂëÊÇ·ñÈ±Ê§
-	IF NOT EXISTS ( SELECT 1 FROM code_transaction WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id 
+
+      -- æ£€æŸ¥è´¹ç”¨ç æ˜¯å¦ç¼ºå¤±
+	IF NOT EXISTS ( SELECT 1 FROM code_transaction WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id
 								AND arrange_code <> '98'  AND CODE NOT IN (SELECT CODE FROM role_table_2017 GROUP BY CODE)) THEN
-		SELECT '·ÑÓÃÂëÎŞÈ±Ê§'  ;
+		SELECT 'è´¹ç”¨ç æ— ç¼ºå¤±'  ;
 	ELSE
-		SELECT * FROM code_transaction WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND arrange_code <> '98'  
+		SELECT * FROM code_transaction WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND arrange_code <> '98'
 			AND is_halt = 'F' AND CODE NOT IN (SELECT CODE FROM role_table_2017 GROUP BY CODE);
 	END IF ;
-	
-      -- ¼ì²éÊĞ³¡ÂëÊÇ·ñÈ±Ê§      
-	IF NOT EXISTS (SELECT 1 FROM code_base WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id 
-							AND  parent_code = 'market_code' AND CODE NOT IN (SELECT CODE FROM role_table_2017 GROUP BY CODE)) THEN 
-		SELECT 'ÊĞ³¡ÂëÎŞÈ±Ê§'  ;    
+
+      -- æ£€æŸ¥å¸‚åœºç æ˜¯å¦ç¼ºå¤±
+	IF NOT EXISTS (SELECT 1 FROM code_base WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id
+							AND  parent_code = 'market_code' AND CODE NOT IN (SELECT CODE FROM role_table_2017 GROUP BY CODE)) THEN
+		SELECT 'å¸‚åœºç æ— ç¼ºå¤±'  ;
 	ELSE
-		SELECT * FROM code_base WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND parent_code = 'market_code' 
+		SELECT * FROM code_base WHERE hotel_group_id = arg_hotel_group_id AND hotel_id = arg_hotel_id AND parent_code = 'market_code'
 			AND CODE NOT IN (SELECT CODE FROM role_table_2017 GROUP BY CODE);
 	END IF ;
-	
+
 	SELECT * FROM role_table_2016;
-      
+
 END$$
 
 DELIMITER ;
