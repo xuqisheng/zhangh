e4af231efb693848c3dc498fac488880b07caa38
zhangh <huilead@126.com>
2017-04-01 13:48:45 +0800

    Ys

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,15 @@
 #!/etc/bin/env python
 # coding:utf-8
 
-
 from fabric.api import local, settings, abort
 from fabric.contrib.console import confirm
 from fabric.api import *
+
+env.user = 'root'
+env.hosts = ['192.168.0.28']
+env.port = 3305
+# env.password='xxxxxx'
+env.key_filename = "/root/.ssh/huiRsa"
 
 # hello world!!!
 def hello():
@@ -16,8 +21,8 @@ def deploy():
     pass
 
 # 上传本地文件到远程主机
-def put_file(source_file,object_file):
-    pass
+def put_file():
+    put('/root/zhangh/psping','/root/')
 
 # 从远程主机下文件到本地
 def get_file():
