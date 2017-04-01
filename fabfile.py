7ffa1f153284dfa8c0283ce58d01f222a1052da6
zhangh <huilead@126.com>
2017-04-01 16:09:28 +0800

    Ys

--- fabfile.py
+++ fabfile.py
@@ -5,25 +5,52 @@ from fabric.api import local, settings, abort
 from fabric.contrib.console import confirm
 from fabric.api import *
 
-env.user = 'root'
-env.hosts = ['192.168.0.28']
-env.port = 3305
-# env.password='xxxxxx'
-env.key_filename = "/root/.ssh/huiRsa"
+env.user  = 'root'
+env.hosts = ['192.168.12.178']
+env.port  = 22
+env.password='action'
+# env.key_filename = "/root/.ssh/huiRsa"
+# env.key_filename = "D:\Python27\huiRsa"
 
 # hello world!!!
 def hello():
     if confirm("Tests failed. Continue anyway?"):
         print "Hello World!!!"
 
-# 部署函数
-def deploy():
-    pass
+# 部署 mode
+def deploy_mode():
+    get_mode()
+    exec_mode()
+
+# 下载 mode 并 解压
+def get_mode():
+    if confirm("DownLoad recent mode & packages?"):
+        with cd('/'):
+            run('wget http://www.ipms.cn:8090/mode.tar.gz')
+            run('wget http://www.ipms.cn:8090/packages.tar.gz')
+            run('tar zxvf mode.tar.gz')
+            run('tar zxvf packages.tar.gz')
+
+# 执行 mode 安装
+def exec_mode():
+    put('D:\Python27\zhangh\\abc', '/')
+    # with cd('/'):
+    #     run('chmod u+x mntOS')
+    #     run('/mntOS')
+
+        # run('/root/mode/settz')
+        # run('/root/mode/disusb')
+        # run('/root/mode/install.sh')
+        # run('/root/mode/inst_mysql')
+        # run('/root/mode/config_mem max')
+        #
+        # run('rm -Rf /root/mode/')
+
 
 # 上传本地文件到远程主机
 def put_file():
-    put('/root/zhangh/psping','/root/')
+    put('D:\Python27\zhangh\pstest','/root/')
 
 # 从远程主机下文件到本地
 def get_file():
-    print 'abc'
+    get('/root/mode/dump/portal_member-2017-02-20_001.sql','D:\Python27')
