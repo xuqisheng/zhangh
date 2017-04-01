709b5edaa0fbb60af4b12c98fb195bea17458feb
zhangh <huilead@126.com>
2017-04-01 13:32:42 +0800

    Ys

--- fabfile.py
+++ fabfile.py
@@ -7,8 +7,6 @@ from fabric.contrib.console import confirm
 from fabric.api import *
 
 # hello world!!!
-print env.user,env.host
-
 def hello():
     if confirm("Tests failed. Continue anyway?"):
         print "Hello World!!!"
@@ -18,9 +16,9 @@ def deploy():
     pass
 
 # 上传本地文件到远程主机
-def put_file():
-    pass
+def put_file(source_file,object_file):
+    print source_file ,'<-->',object_file
 
 # 从远程主机下文件到本地
 def get_file():
-    pass
+    print 'abc'
