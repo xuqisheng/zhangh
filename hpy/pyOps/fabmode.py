930c5eb321bbd4a1aaca4333614c7f77e64d1e29
zhangh <huilead@126.com>
2017-04-21 06:43:17 +0800

    Ys

--- hpy/pyOps/fabmode.py
+++ hpy/pyOps/fabmode.py
@@ -43,5 +43,34 @@ def getip():
 def shutdown():
     run("shutdown -h now")
 
+# 部署 mode
+def deploy_mode():
+    get_mode()
+    exec_mode()
 
+# 下载 mode 并 解压
+def get_mode():
+    if confirm("DownLoad recent mode & packages?"):
+        with cd('/'):
+            run('wget http://www.ipms.cn:8090/mode.tar.gz')
+            run('wget http://www.ipms.cn:8090/packages.tar.gz')
+            run('tar zxvf mode.tar.gz')
+            run('tar zxvf packages.tar.gz')
+
+# 执行 mode 安装
+def exec_mode():
+    # 上传本地文件，用于CentOS6.4版本及以上的基础组件安装
+    put('D:\Python27\zhangh\hpy\pyBase\mntOS.py', '/root')
+    run('python /root/mntOS.py')
+
+    run('/root/mode/settz')
+    run('/root/mode/disusb')
+    run('/root/mode/install.sh')
+    run('/root/mode/inst_mysql')
+    run('/root/mode/config_mem max')
+
+    # 删除上传的本地文件
+    run('rm -Rf /root/mntOS.py')
+
+    print '^_^ Congratulations! Mode deployment is successful!'
 
