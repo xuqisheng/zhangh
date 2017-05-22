440494fe34c50e6bed8063e86612f0dd22119482
zhangh <huilead@126.com>
2017-05-22 22:05:29 +0800

    Ys

---
+++ hpy/pyOps/PyMntOs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding:utf8
+
+"""
+@version:
+@author :zhangh
+@file   :pyLinuxBase.py
+@time   :2017/5/22 13:52
+@remark :使用python脚本维护Linux基本环境
+"""
+import os,re
+
+
+class PyMntOs(object):
+
+    # yum源更新，来自于阿里云
+    @staticmethod
+    def yum_update(self,releasever):
+        os.system('mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup')
+
+        if float(5) <= releasever < float(6):
+            os.system('wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-5.repo')
+        elif float(6) <= releasever < float(7):
+            os.system('wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo')
+        elif float(7) <= releasever:
+            os.system('wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo')
+
+        os.system('yum makecache')
+
+    # 获取centos的发行版本号
+    @staticmethod
+    def issue_num(self):
+        f = open('/etc/issue', 'r')
+        num = re.search(r'[0-9].[0-9]',f.readline().strip('\n'))
+        f.close()
+
+        return num.group()
+
+    # 安装 ansible 所需的基础环境
+    def ansible_base(self):
+        if os.system('yum install python-devel') != 0:
+            self.yum_update(float(self.issue_num()))
+            os.system('yum install python-devel')
+
+
+if __name__ == '__main__':
+    zhexec = PyMntOs()
+    zhexec.ansible_base()
