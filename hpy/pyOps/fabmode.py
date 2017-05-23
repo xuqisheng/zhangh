562b9ccf44baaa371122f7d4251cb6ce31bc924a
zhangh <huilead@126.com>
2017-05-23 18:44:01 +0800

    zhangh

--- hpy/pyOps/fabmode.py
+++ hpy/pyOps/fabmode.py
@@ -16,7 +16,7 @@ from fabric.api import *
 
 # 单个服务器，通过私钥连接
 env.user = 'root'
-env.hosts = ['218.75.13.109']
+env.hosts = ['182.254.135.193','www.gcihotel.com','182.254.244.164','115.159.190.22','182.254.129.77']
 env.port = 3305
 env.key_filename = "D:\Python27\huiRsa"
 
