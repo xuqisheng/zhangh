59d332a1427e49cdadb51732f29dd6845e81feb1
zhangh <huilead@126.com>
2017-05-25 23:01:30 +0800

    Ys

--- hpy/pyOps/inst_mode.py
+++ hpy/pyOps/inst_mode.py
@@ -21,3 +21,15 @@ else:
 # 关闭文件
 f.close()
 
+# 开始安装mode
+os.system('/root/mode/settz')
+os.system('/root/mode/disusb')
+
+# 如何解决install.sh中read交互输入的问题?
+os.system('export HBLACKBOX2=1;/root/mode/install.sh')
+
+# export HBLACKBOX2=1;用于解决确认执行脚本过程中提交是否输入y/n的问题
+os.system('export HBLACKBOX2=1;/root/mode/inst_mysql')
+os.system('export HBLACKBOX2=1;/root/mode/config_mem max')
+
+
