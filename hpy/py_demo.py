2fa7b4c5928f9cc8fcdaa01f337bfbb309ad5f94
zhangh <huilead@126.com>
2017-06-13 22:52:01 +0800

    zhangh

--- hpy/py_demo.py
+++ hpy/py_demo.py
@@ -115,9 +115,9 @@ print os.path.isfile(filename)
 # print a,'<-->',b
 
 
+# import win32api,win32con
+# win32api.MessageBox(win32con.NULL, u'Python 你好！', u'你好', win32con.MB_OK)
 
+from ctypes import *
 
-
-import win32api,win32con
-win32api.MessageBox(win32con.NULL, u'Python 你好！', u'你好', win32con.MB_OK)
-
+print c_int(0)
