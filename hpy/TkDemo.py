4b958c84730651a5843b5894b01d7bae96e76131
zhangh <huilead@126.com>
2017-06-20 22:38:44 +0800

    zhangh

---
+++ hpy/TkDemo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding:utf8
+
+"""
+@version:
+@author :zhangh
+@file   :TkDemo.py
+@time   :2017/6/20 22:26
+@remark :
+"""
+from Tkinter import *
+
+root = Tk()
+root.title('hello Tkinter')
+label = Label()
+label.pack()
+root.mainloop()
--- hpy/pySQL/mysql_connect_demo.py
+++ hpy/pySQL/mysql_connect_demo.py
@@ -2,6 +2,8 @@
 #coding:utf-8
 
 import mysql.connector
+
+
 config = {
 	'user':'root',
 	'password':'deviskaifa',
