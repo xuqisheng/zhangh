92b393a028108bc14784c06d1e09886a2854b476
zhangh <huilead@126.com>
2017-06-20 23:25:44 +0800

    zhangh

--- hpy/TkDemo.py
+++ hpy/TkDemo.py
@@ -10,8 +10,20 @@
 """
 from Tkinter import *
 
+def say_hello():
+    print "Hello"
+
+
+# 根窗口
 root = Tk()
-root.title('hello Tkinter')
-label = Label()
-label.pack()
+root.title('Tkdemo 事例')
+root.geometry('600x380+300+300')
 root.mainloop()
+
+entry = Entry(root)  # 创建一个输入框 布局：显示的方式和位置
+entry.pack()
+button = Button(root,text='搜 索',command=say_hello)
+button.pack()
+
+if __name__ == '__main__':
+    pass
