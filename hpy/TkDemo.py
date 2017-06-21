2f157a4a841d05c4bbf995db82085f0d2ffd937a
zhangh <huilead@126.com>
2017-06-21 19:43:40 +0800

    zhangh

--- hpy/TkDemo.py
+++ hpy/TkDemo.py
@@ -10,30 +10,34 @@
 """
 from Tkinter import *
 
+class TkMySQL(object):
+    def __init__(self,root):
+        self.root = root
+
 
 def say_hello():
-    print "Hello"
-
-
-# 根窗口
-root = Tk()
-root.title('Tkdemo 事例')
-root.geometry('600x380+300+300')
-
-
-# entry = Entry(root)  # 创建一个输入框 布局：显示的方式和位置
-# entry.pack()
-button = Button(root,text='增 加',command=say_hello)
-button.pack(side=RIGHT)
-
-button = Button(root,text='删 除',command=say_hello)
-button.pack(side=BOTTOM)
-
-# 事件循环
-root.mainloop()
+        print "hello"
 
 
 if __name__ == '__main__':
-    pass
+    # 根窗口
+    root = Tk()
+    root.title('Tkdemo 事例')
+    root.geometry('600x380+300+300')
+
+    Label(root, text="酒店代码").place(x=350, y=10, width=60, height=28)
+    Label(root, text="营业点").place(x=350, y=48, width=60, height=28)
+
+    Entry(root).place(x=410, y=10, width=90, height=28)
+    Entry(root).place(x=410, y=48, width=90, height=28)
+
+    Button(root, text='查 询', command=say_hello).place(x=200, y=20, width=60, height=28)
+    Button(root, text='删 除').place(x=510, y=10, width=60, height=28)
+    Button(root, text='增 加').place(x=510, y=45, width=60, height=28)
+
+    Listbox(root).pack()
+
+    # 事件循环
+    root.mainloop()
 
 
