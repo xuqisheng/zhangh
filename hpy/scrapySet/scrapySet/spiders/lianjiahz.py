d1adfd5d4b6ebb399fe462a06c9d984bfc675d6b
zhangh <huilead@126.com>
2017-06-23 21:58:49 +0800

    zhangh

--- hpy/scrapySet/scrapySet/spiders/lianjiahz.py
+++ hpy/scrapySet/scrapySet/spiders/lianjiahz.py
@@ -8,3 +8,11 @@
 @time   :2017/6/22 21:52
 @remark : 杭州链家二手房
 """
+import scrapy
+from scrapy.spider import CrawlSpider
+
+class LianjiaHz(CrawlSpider):
+
+    name = "lianjiahz"
+    allowed_domains = ["hz.lianjia.com"]
+    start_urls = ('http://hz.lianjia.com/ershoufang/')
