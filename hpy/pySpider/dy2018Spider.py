ba87a0db574e8b5940209344367cf71396ef9860
zhangh <huilead@126.com>
2017-07-11 22:26:11 +0800

    zhangh

--- hpy/pySpider/dy2018Spider.py
+++ hpy/pySpider/dy2018Spider.py
@@ -8,34 +8,37 @@
 @time   :2017/7/10 21:50
 @remark : 对电影天堂进行爬虫，获取相关信息，比如 电影名称、下载地址、更新日期、点击次数
 """
-
+import os
+import pandas
 import requests
 import sys
 from bs4 import BeautifulSoup
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 url_init = 'http://www.dy2018.com/html/gndy/dyzz/index.html'
+url_index = 'http://www.dy2018.com'
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/536.5 '
                          '(KHTML, like Gecko) Chrome/19.0.1084.54 Safari/536.5'}
-r = requests.get(url_init, headers=headers)
-r.encoding = 'utf-8'
-content = r.text
+response = requests.get(url_init, headers=headers)
+response.encoding = 'gb2312'
+content = response.text
 
 soup = BeautifulSoup(content, 'html.parser')
 
-# items = soup.find_all('div',class_='co_content8')
 items = soup.find_all('a', class_='ulink')
-print items
+
 ret = []
 for item in items:
-    # title = item.a.get_text()
-    print type(item)
-    # ret.append(title)
+    info = {}
+    title = item['title']
+    info['titel'] = title.strip()
+    href = item['href']
+    info['href'] = url_index + href.strip()
+    ret.append(info)
 
-print ret
+if os.path.exists('dy2018.xlsx'):
+    os.remove('dy2018.xlsx')
 
-# if __name__ == '__main__':
-#     get_url = 'http://www.dy2018.com/html/gndy/dyzz/index.html'
-#     page = gethtml(get_url)
+df = pandas.DataFrame(ret)
+df.to_excel('dy2018.xlsx')
+
+
