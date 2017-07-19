2023d7d4fa4707ae3cde30c4791e9e404494656f
zhangh <huilead@126.com>
2017-07-19 22:55:12 +0800

    zhangh

--- iPark/iLion/models.py
+++ iPark/iLion/models.py
@@ -6,3 +6,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
