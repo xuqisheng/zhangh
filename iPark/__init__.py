78b41d6e86af0e8c665b79415bcbf3fd48e48d6e
zhangh <huilead@126.com>
2017-07-21 15:07:59 +0800

    zhangh

---
+++ iPark/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding:utf8
+
+"""
+@version:
+@author :zhangh
+@file   :__init__.py.py
+@time   :2017/7/21 14:02
+@remark :
+"""
--- iPark/iLion/urls.py
+++ iPark/iLion/urls.py
@@ -9,15 +9,10 @@
 @remark :
 """
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
     # ex: /iLion/
     url(r'^$', views.index),
-    # ex: /iLion/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /iLion/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /iLion/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
