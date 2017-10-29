6d4f3dcd407734bf51f1855c37c96529b8cec122
zhangh <huilead@126.com>
2017-10-29 12:01:33 +0800

    zhangh

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^hello$', views.hello, name='hello'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
