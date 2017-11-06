730b48e921e125f8f572cc9f2be572acbbd6d888
zhangh <huilead@126.com>
2017-11-06 22:59:24 +0800

    zhangh

--- hpy/pymode/mod_config.py
+++ hpy/pymode/mod_config.py
@@ -10,7 +10,7 @@
           对配置文件配置的添加和修改
 """
 import sys
-import os
+import os, re
 
 Hparams = sys.argv[1:]
 Hconfigfile = Hparams[0]
@@ -22,14 +22,19 @@ Hafteritem  = Hparams[2]
 
 # 检查配置文件是否存在
 if Hconfigfile is None:
-    print("Configuration file not provided!")
+    print("Configuration file not provided!") & exit(1)
 elif not os.path.isfile(Hconfigfile):
-    print("File " + Hconfigfile + " doesn't exist!")
+    print("File " + Hconfigfile + " doesn't exist!") & exit(1)
 
 # 检查参数有效性
 if Hconfigitem is None:
-    print("Configuration item not provided!")
-elif expression:
-    pass
+    print("Configuration item not provided!") & exit(1)
+elif re.compile(r'=.*=').search(Hconfigitem):
+    print("More than 1 \"=\" in Configuration item is not allowed!") & exit(1) 
 
+# 提取Hconfigitem中等号前后内容
+abc = re.compile(r'(^.*)=').search(Hconfigitem)
+print(abc.group(0))
 
+abd = re.compile(r'=.*').search(Hconfigitem)
+print(abd.group(0))
