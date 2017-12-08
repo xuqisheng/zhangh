8cd78579caecb34d06ccd7f2568fd5549014e80f
zhangh <huilead@126.com>
2017-12-08 21:40:50 +0800

    zh

--- hpy/pySpider/urllibSpiderSet.py
+++ hpy/pySpider/urllibSpiderSet.py
@@ -5,22 +5,23 @@
 @version:
 @author :zhangh
 @time   :2017/12/7
-@remark : 爬取京东手机图片
+@remark : urllib编写的简单爬虫示例集合
 """
 
 import re
 import urllib.request
 
 
-def craw(url, page):
+# 爬取京东图片
+def jd_craw_photo(url, page):
     headers = ("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3831.407 Safari/537.36")
     opener = urllib.request.build_opener()
     opener.addheaders = [headers]
-    html1 = str(opener.open(url).read())
-    pattern1 = '<div id="plist".+? <div class="page clearfix">'
-    result1 = re.compile(pattern1).findall(html1)[0]
-    pattern2 = '<img width="220" height="220" data-img="1" src="//(.+?\.jpg)"'
-    imagelist = re.compile(pattern2).findall(result1)
+    html_str = str(opener.open(url).read())
+    pattern_plist = '<div id="plist".+? <div class="page clearfix">'
+    result_plist = re.compile(pattern_plist).findall(html_str)[0]
+    pattern_jpg = '<img width="220" height="220" data-img="1" src="//(.+?\.jpg)"'
+    imagelist = re.compile(pattern_jpg).findall(result_plist)
     x = 1
     for imageurl in imagelist:
         imagename = "d://myweb/"+str(page)+str(x)+".jpg"
@@ -34,7 +35,24 @@ def craw(url, page):
                 x += 1
         x += 1
 
-for i in range(1,10):
-    # url = "https://list.jd.com/list.html?cat=9987,653,655&page="+str(i)
-    url = "https://list.jd.com/list.html?cat=9987,653,655&page="+str(i)
-    craw(url,i)
+# 爬取CSDN的blog主页所有链接
+def csdn_craw_link(url):
+    headers = ("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3831.407 Safari/537.36")
+    opener = urllib.request.build_opener()
+    opener.addheaders = [headers]
+    html_str = str(opener.open(url).read())
+    pat = '(https?://[^/s";]+\.(\w|/)*)'
+    link = re.compile(pat).findall(html_str)
+    #去除重复元素
+    link = list(set(link))
+    return link
+
+if __name__ == '__main__':
+    # 京东
+    # for i in range(1,10):
+    #     url = "https://list.jd.com/list.html?cat=9987,653,655&page="+str(i)
+    #     jd_craw_photo(url,i)
+
+    # csdn
+    for link in csdn_craw_link("http://www.sina.com.cn"):
+        print(link[0])
