ccc3056164206bd87b7796d33fab029c28633b4f
zhangh <huilead@126.com>
2017-12-08 20:34:03 +0800

    zhangh

--- hpy/pySpider/SpiderJdMobliePhoto.py
+++ hpy/pySpider/SpiderJdMobliePhoto.py
@@ -13,7 +13,10 @@ import urllib.request
 
 
 def craw(url, page):
-    html1 = str(urllib.request.urlopen(url).read())
+    headers = ("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3831.407 Safari/537.36")
+    opener = urllib.request.build_opener()
+    opener.addheaders = [headers]
+    html1 = str(opener.open(url).read())
     pattern1 = '<div id="plist".+? <div class="page clearfix">'
     result1 = re.compile(pattern1).findall(html1)[0]
     pattern2 = '<img width="220" height="220" data-img="1" src="//(.+?\.jpg)"'
@@ -32,5 +35,6 @@ def craw(url, page):
         x += 1
 
 for i in range(1,10):
+    # url = "https://list.jd.com/list.html?cat=9987,653,655&page="+str(i)
     url = "https://list.jd.com/list.html?cat=9987,653,655&page="+str(i)
     craw(url,i)
