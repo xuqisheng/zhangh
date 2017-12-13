0295d09b0175ef2e16bfc0595b4130ba06f69c02
zhangh <huilead@126.com>
2017-12-13 22:16:45 +0800

    zhangh

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question, Choice
+from .models import Question
+
+
 admin.site.register(Question)
-admin.site.register(Choice)
--- mysite_old/polls/models.py
+++ mysite_old/polls/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.utils import timezone
-import datetime
 
 # Create your models here.
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,11 +10,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
